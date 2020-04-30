974f559f68d6b0480e8057cb0988063a8415646a
Naveen Kumar K <knaveenkumar3576@gmail.com>
2020-04-30 12:34:51 -0400

    Updates

---
+++ precache-manifest.5afe5410d425e2b8ad2d88bcc0a95475.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9c3267eefa405e7571385a67339aef",
+    "url": "/mini-bc/index.html"
+  },
+  {
+    "revision": "350c6a9f7060ea41ed25",
+    "url": "/mini-bc/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "07110ca66bf6c763407a",
+    "url": "/mini-bc/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "350c6a9f7060ea41ed25",
+    "url": "/mini-bc/static/js/2.07b4e9ac.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mini-bc/static/js/2.07b4e9ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07110ca66bf6c763407a",
+    "url": "/mini-bc/static/js/main.84d5dea5.chunk.js"
+  },
+  {
+    "revision": "aacc31ef1751a8987b79",
+    "url": "/mini-bc/static/js/runtime-main.8051f8cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mini-bc/static/media/logo.5d5d9eef.svg"
+  }
+]);
